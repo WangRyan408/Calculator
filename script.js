e3040a49f87e01944b7fe20f0fd05548bce5c116
Wangstaboi <bubmom9@gmail.com>
2022-08-26 21:49:20 -0700

    Did some more work on the UI

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 //Calculate stuff!
+const btn = document.querySelectorAll('.btn');
 
-function add(a, b) {
-    return a + b;
+function calculate(a, b) {
+
+    function add(a, b) {
+        return a + b;
+    }
+
+    function subtract(a, b) {
+        return a - b;
+    }
+
+    function multiply(a, b) {
+        return a * b;
+    }
+
+    function divide(a, b) {
+        return a / b;
+    }
+
 }
 
-function subtract(a, b) {
-    return a - b;
+function buttonSize() {
+    for (let i = 0; i < btn.length; i++) {
+        btn[i].setAttribute('id', `btn-${i}`);
+    }
 }
 
-function multiply(a, b) {
-    return a * b;
-}
 
-function divide(a, b) {
-    return a / b;
-}
+buttonSize();
+console.log(btn);
